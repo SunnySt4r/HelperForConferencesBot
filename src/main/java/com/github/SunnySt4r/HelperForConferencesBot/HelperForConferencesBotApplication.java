a38ef5addb5dac30e7595b6ce63fb1a41b432a61
SunnySt4r <door011@mail.ru>
2022-08-31 22:32:00 +0300

    Commit 2

---
+++ src/main/java/com/github/SunnySt4r/HelperForConferencesBot/HelperForConferencesBotApplication.java
@@ -0,0 +1,15 @@
+package com.github.SunnySt4r.HelperForConferencesBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperForConferencesBotApplication {
+
+	public static void main(String[] args) {
+
+		//s
+		SpringApplication.run(HelperForConferencesBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/SunnySt4r/HelperForConferencesBot/HelperForConferencesBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.SunnySt4r.HelperForConferencesBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperForConferencesBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
